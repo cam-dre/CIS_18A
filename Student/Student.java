8fb5abe42241f61444c6df1ceef2e1ddd31543e3
cam-dre <37496350+cam-dre@users.noreply.github.com>
2018-03-20 11:24:13 -0700

    Add files via upload

--- Student/Student.java
+++ Student/Student.java
@@ -1,50 +1,20 @@
-import java.text.DecimalFormat;
-
 public class Student {
+	private int id;
 	private String name;
-	private String ssn;
-	private double gpa;
-	private static int studentCount = 0;
+	private int age;
 	
-	public Student() {
-		studentCount++;
+	public Student(int id, String name, int age) {
+		this.id = id;
+		this.name  = name;
+		this.age = age;
 	}
-	public Student(String name, String ssn, double gpa) {
-		this.name = name;
-		this.ssn = ssn;
-		this.gpa = gpa;
-		studentCount++;
+	public int getNumber() {
+		return id;
 	}
-	public String getName() {
+	public String getName() { 
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getSsn() {
-		return ssn;
-	}
-	public void setSsn(String ssn) {
-		this.ssn = ssn;
-	}
-	public double getGpa() {
-		return gpa;
-	}
-	public void setGpa(double gpa) {
-		// gpa must be between 0.0 and 4.0 or method will display a runtime error
-		if (gpa > 0.0 && gpa <= 4.0)
-			this.gpa = gpa;
-		else {
-			System.err.println("Gpa must be between 0.0 and 4.0.");
-			System.err.println("Value not changed.\n");
-		}
-	}
-	public static int getCount() {
-		return studentCount;
-	}
-	public String toString() {
-		DecimalFormat oneDigit = new DecimalFormat("0.0");
-		return "Name: " + name + "; SSN: " + ssn + "; GPA: " + oneDigit.format(gpa)
-				+ "\n";
+	public int getAge() {
+		return age;
 	}
 }
--- Student/StudentClient.java
+++ Student/StudentClient.java
@@ -1,68 +1,41 @@
 // =================================================================================
-// Attached: HW_6a, 6b, 6c
-// =================================================================================
-// File: HW_6a - Students
+// File: Extra Credit
 // =================================================================================
 // Programmer: Cameron Perry
 // Class: CIS 18A
 // =================================================================================
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class StudentClient {
 	public static void main(String[] args) {
-		Student s1 = new Student();
-		Student s2 = new Student("Smith", "123-45-6789", 3.2);
-		Student s3 = new Student("Jones", "987-65-4321", 3.7);
+		// Create Student objects
+		Student s1 = new Student(1000, "Kim", 19);
+		Student s2 = new Student(1001, "Bob", 22);
+		Student s3 = new Student(1002, "Joe", 21);
 		
-		System.out.println("Default constructor was used for student #1.");
-		// toString method used to display student_1
-		System.out.println("Student #1 is " + s1);
+		// Create array list
+		ArrayList<Student> list = new ArrayList<Student>();
 		
-		// getSsn method used to get the ssn for student_1
-		System.out.println("The social securtiy number of student #1 is " + s1.getSsn() + "\n");
+		// Put the Student objects in the list
+		list.add(s1);
+		list.add(s2);
+		list.add(s3);
 		
-		// toString method used to display student_2
-		System.out.println("Student #2 is " + s2);
+		// Assign the iterator to the beginning of the list
+		Iterator it = list.iterator();
 		
-		// getName method used to get the name of student_3
-		System.out.println("The name of student #3 is " + s3.getName());
-		
-		// getSsn method used to get the ssn for student_3
-		System.out.println("The social securtiy number of student #3 is " + s3.getSsn());
-		
-		// getGpa method used to get the gpa of student_3
-		System.out.println("The gpa of student #3 is " + s3.getGpa());
-		
-		// toString method used to display student_3
-		System.out.println("Student #3 is " + s3);
-		
-		System.out.print("Now set the gpa of Student #2 to 5.0:\n");
-		// setGpa method used to change the gpa for student_2
-		s2.setGpa(5.0);
-		
-		// toString method used to display student_2
-		System.out.println("Student #2 is " + s2);
-		
-		// getCount method used to return the number of students 
-		System.out.println("There are " + Student.getCount() + " students.");
+		// Traverse the list elements
+		while (it.hasNext()) {
+			Student student =  (Student)it.next();
+			System.out.println(student.getNumber() + " " + student.getName() 
+					+ " " + student.getAge());
+		}
 	}
 }
 
 /* ==== OUTPUT =====================================================================
-Default constructor was used for student #1.
-Student #1 is Name: null; SSN: null; GPA: 0.0
-
-The social securtiy number of student #1 is null
-
-Student #2 is Name: Smith; SSN: 123-45-6789; GPA: 3.2
-
-The name of student #3 is Jones
-The social securtiy number of student #3 is 987-65-4321
-The gpa of student #3 is 3.7
-Student #3 is Name: Jones; SSN: 987-65-4321; GPA: 3.7
-
-Now set the gpa of Student #2 to 5.0:
-Student #2 is Name: Smith; SSN: 123-45-6789; GPA: 3.2
-
-There are 3 students.
-Gpa must be between 0.0 and 4.0.
-Value not changed.
-====================================================================================*/
+1000 Kim 19
+1001 Bob 22
+1002 Joe 21
+==================================================================================*/
