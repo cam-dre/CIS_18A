5176818309c221b31a989fcb681e9b7336b9ca98
cam-dre <37496350+cam-dre@users.noreply.github.com>
2018-03-20 18:09:55 -0700

    Add files via upload

--- Random Numbers + Math object/WhileLoop.java
+++ Random Numbers + Math object/WhileLoop.java
@@ -5,7 +5,6 @@ public class WhileLoop {
 	public static void main(String[] args) {
 		double num;
 		char choice = 'Y';
-		String strChoice;
 		
 		Scanner scan = new Scanner(System.in);
 		DecimalFormat twoDigits = new DecimalFormat("###.00");
@@ -17,9 +16,7 @@ public class WhileLoop {
 			System.out.println(num + " squared equals " + twoDigits.format(Math.pow(num, 2)));
 			
 			System.out.print("Do again (Y or N)?  ");
-			strChoice = scan.next();
-			strChoice = strChoice.toUpperCase();
-			choice = strChoice.charAt(0);
+			choice = scan.next().toUpperCase().charAt(0);
 		}
 		scan.close();
 	}
